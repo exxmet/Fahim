500be0af897edf85b9865ff5649831fa4a394bbe
exxmet <fi9089298@gmail.com>
2025-10-16 00:54:28 +0600

    Update info.js

--- scripts/commands/info.js
+++ scripts/commands/info.js
@@ -36,12 +36,12 @@ var callback = () => api.sendMessage({body:`
 𝐆𝐞𝐧𝐝𝐞𝐫.   : 𝐌𝐚𝐥𝐞
 𝐀𝐠𝐞           : 20+
 𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧𝐬𝐡𝐢𝐩 : 𝐒𝐢𝐧𝐠𝐥𝐞
-𝐖𝐨𝐫𝐤        : 𝐒𝐭𝐮𝐝𝐞𝐧𝐭
+𝐖𝐨𝐫𝐤        : job
 𝐆𝐦𝐚𝐢𝐥       : Fahimbot98@gmail.com
 𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩: wa.me/+8801892323664
 𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦  : ওই সব বাল চালাই না😡
 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 : https://www.facebook.com/☞♥♪✆😘😍🥵🤯 Fahim islam ☞♥♪✆😘😍🥵🤯`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
     fs.unlinkSync(__dirname + "/cache/1.png"));  
-      return request(encodeURI(`https://graph.facebook.com/100087594397730/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(
+      return request(encodeURI(`https://graph.facebook.com/61582468523117/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(
 fs.createWriteStream(__dirname+'/cache/1.png')).on('close',() => callback());
    };
